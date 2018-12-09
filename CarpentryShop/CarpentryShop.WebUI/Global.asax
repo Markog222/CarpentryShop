660ab3664da7faa15a575e83fac6974e485993dc
Markog222 <mark.speaktome@gmail.com>
2018-12-09 19:44:28 +0000

    Intital Solution Set Up

---
+++ CarpentryShop/CarpentryShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarpentryShop.WebUI.MvcApplication" Language="C#" %>
---
+++ CarpentryShop/CarpentryShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarpentryShop.WebUI.Startup))]
+namespace CarpentryShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
